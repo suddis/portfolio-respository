f39302b5ca61b023adc75e093f556b217826f83d
suddis <suddis-aden@hotmail.com>
2026-07-25 23:17:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+﻿const thumbnails = document.querySelectorAll(".thumbnail");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const close = document.getElementById("close");
+
+thumbnails.forEach(image => {
+    image.addEventListener("click", () => {
+        lightbox.style.display = "block";
+        lightboxImg.src = image.src;
+    });
+});
+
+close.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = "none";
+    }
+});
