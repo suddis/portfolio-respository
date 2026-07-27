a80a192f38c9958a8f09a22eeae3916e184a3e5a
suddis <suddis-aden@hotmail.com>
2026-07-26 22:42:37 -0400

    Updated portfolio website

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-﻿const thumbnails = document.querySelectorAll(".thumbnail");
-const lightbox = document.getElementById("lightbox");
-const lightboxImg = document.getElementById("lightbox-img");
-const close = document.getElementById("close");
-
-thumbnails.forEach(image => {
-    image.addEventListener("click", () => {
-        lightbox.style.display = "block";
-        lightboxImg.src = image.src;
-    });
-});
-
-close.addEventListener("click", () => {
-    lightbox.style.display = "none";
-});
-
-lightbox.addEventListener("click", (e) => {
-    if (e.target === lightbox) {
-        lightbox.style.display = "none";
-    }
+﻿document.addEventListener("DOMContentLoaded", function () {
+    console.log("Portfolio website loaded successfully!");
 });
